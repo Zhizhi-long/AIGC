fe400486cb603d65ccd93edfd645a024856a830f
long <long@longdeMacBook-Pro.local>
2025-07-12 13:37:12 +0800

    fix: ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,16 +10,9 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/architectural-render-n',
       name: 'architectural-render-n',
+      // @ts-ignore
       component: () => import('../views/ArchitecturalRender-new.vue')
     }
   ],
